28486cef3f69afede3a0c7693093aa9fe037d8b5
Admin <209695793@qq.com>
2025-02-09 14:41:53 +0800

    添加盒子

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,46 @@
 import * as THREE from "three";
+function main() {
+  const canvas = document.querySelector("#c");
+  console.log("canvas: ", canvas);
+  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+  const fov = 75;
+  const aspect = 2; // 相机默认值
+  const near = 0.1;
+  const far = 5;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+  const scene = new THREE.Scene();
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+  const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
 
-camera.position.z = 5;
+  const color = 0xffffff;
+  const intensity = 3;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(-1, 2, 4);
+  scene.add(light);
+  // renderer.render(scene, camera);
 
-function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+  function render(time: number) {
+    console.log("render: ");
+    time *= 0.001; // 将时间单位变为秒
+    console.log("time: ", time);
+
+    cube.rotation.x = time;
+    cube.rotation.y = time;
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
 }
-animate();
+
+main();
